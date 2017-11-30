137ad39d0196e8058dbcd0fbf9b98f771dd7fcb3
Francis Redido <sicnarfodider@hotmail.com>
2017-11-29 22:14:12 -0800

    render deal dmg is working , need to move on, switching players and getting a new set of functions

--- resources/main.js
+++ resources/main.js
@@ -71,13 +71,13 @@ function GameModel(){
         'libertybelle' : {
             name: 'Liberty Belle',
             img: 'liberty-belle.png',
-            category: 'Math',
+            category: "History",
             categoryID: '19'
         },
         'thething' : {
             name: 'The Thing',
             img: 'thing.png',
-            category: 'Computers',
+            category: "Science & Nature",
             categoryID: '18'
         },
         'mrfantastic' : {
@@ -89,7 +89,7 @@ function GameModel(){
         'batman' : {
             name: 'Batman',
             img: 'batman.png',
-            category: 'Gadgets',
+            category: "Science: Gadgets",
             categoryID: '9'
         },
         'ironman' : {
@@ -107,13 +107,13 @@ function GameModel(){
         'nightcrawler' : {
             name: 'Nightcrawler',
             img: 'nightcrawler.png',
-            category: 'Video Games',
+            category: "Entertainment: Video Games",
             categoryID: '15'
         },
         'wonderwoman' : {
             name: 'wonderwoman',
             img: 'wonderwoman.png',
-            category: 'Animals',
+            category: "Art",
             categoryID: '27'
         },
         'juggernaut' : {
@@ -125,16 +125,16 @@ function GameModel(){
         'mrsinister' : {
             name: 'Mr. Sinister',
             img: 'mr-sinister.png',
-            category: 'History',
+            category: "Science & Nature",
             categoryID: '23'
         },
         'robin' : {
             name: 'Robin',
             img: 'robin.png',
-            category: 'Comics',
+            category: "Entertainment: Comics",
             categoryID: '29'
         }
-    }
+    };
 
 
     this.addPlayer = function(character){
@@ -219,21 +219,22 @@ function View(model){
         });
         $('.questionContainer p').text(question).append(catSpan);
         for(var ans_i=0;ans_i<ansList.length;ans_i++){
-            this.createAnsDiv(ans_i, correctAns, ansList[ans_i], category);
+            this.createAnsDiv(ans_i,ansList[ans_i], entry);
         }
         if(model.questionsLeft===0){
             //wincheckstate & player change
             controller.winCheck();
         }
     };
-    this.createAnsDiv=function(num, answer, text, category){
+    this.createAnsDiv=function(num,text, entry){
         var ansDiv= $('<div>',{
             id: 'q'+num,
             'class': 'answer',
             text: controller.domParser(text)
         });
-        ansDiv[0].category = category;
-        if(text!==answer){ //stores correct and incorrect properties inside the DOM element
+        ansDiv[0].difficulty = entry.difficulty;
+        ansDiv[0].category = entry.category;
+        if(text!==entry.correct_answer){ //stores correct and incorrect properties inside the DOM element
             ansDiv[0].answer= 'incorrect';
         }else{
             ansDiv[0].answer = 'correct'
@@ -243,21 +244,24 @@ function View(model){
     this.renderDmg = function(amount){
         var percent = amount/100;//get percent equivalent of the dmg
         var hpBar=null;
-        var remainingHp=null;
+        var hp=null;
         var dmg=null;
+        var remainingHp=null
         if(model.turn === 1){
             hpBar = $('.right');
 
         }else{
             hpBar = $('.left')
         }
-        remainingHp = hpBar.css('width')
-        if(remainingHp-amount<0){
-            dmg=0;
+        var hpBar2 = hpBar.css('width');
+        hp = parseInt(hpBar2.substring(0,hpBar2.indexOf('p')));
+        dmg = Math.round(hp*percent);
+        if(hp-dmg<0){
+            remainingHp=0;
         }else{
-            dmg=remainingHp-amount
+            remainingHp=hp-dmg
         }
-        hpBar.css('width', dmg+"%") //reduces the width by the percentage of the dmg.
+        hpBar.css('width', remainingHp+"px") //reduces the width by the percentage of the dmg.
     };
 
   //
@@ -338,7 +342,7 @@ function Controller(model,view){
     ? model.players[model.turn + 1]['hitPoints'] -= amount
     : model.players[model.turn - 1]['hitPoints'] -= amount;
     view.renderDmg(amount);
-  }
+  };
   this.dmgCalculator = function(difficulty, boolean){
       var damagePercent = 0;
       if(boolean){
@@ -346,23 +350,23 @@ function Controller(model,view){
       }
       switch (difficulty){
           case 'easy':
-              damagePercent+=4;
+              damagePercent+=20;
               break;
           case 'medium':
-              damagePercent+=8;
+              damagePercent+=25;
               break;
           case 'hard':
-              damagePercent+=12;
+              damagePercent+=30;
               break;
       }
       return damagePercent
-  }
+  };
 
     var view = null;
     this.setView = function (viewer) {
         view = viewer;
         delete this.setView;
-    }
+    };
 
 
   this.getSessionToken = function(){
@@ -384,7 +388,7 @@ function Controller(model,view){
               console.log('error input');
           }
       });
-  }
+  };
 
   this.checkWinState = function() {
       if (model.players['1']['hitPoints'] <= 0 || model.players['2']['hitPoints'] <= 0) {
@@ -399,7 +403,7 @@ function Controller(model,view){
           }
           controller.questionBank(model.questions)
       }
-  }
+  };
 
       this.retrieveQuestions = function (diff) {
           $.ajax({
@@ -424,8 +428,7 @@ function Controller(model,view){
                   console.log('error input')
               }
           });
-      }
-
+      };
       this.buildQuestionShoe = function () {
           console.log('build shoe');
           var difficulty = ['easy', 'medium', 'hard'];
@@ -463,10 +466,10 @@ function Controller(model,view){
           var specialty = false;
 
           if (element.answer === 'correct') {
-              if (element.category === model.player[model.turn].character.category) {
+              if (element.category === model.players[model.turn].character.category) {
                   specialty = true;
-                  this.dealDamage(this.dmgCalculator(difficulty, specialty))
               }
+              this.dealDamage(this.dmgCalculator(element.difficulty, specialty));
           }
           view.renderQuestion(model.questionBank);
       };
